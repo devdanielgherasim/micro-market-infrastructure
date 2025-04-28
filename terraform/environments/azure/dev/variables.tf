b0b27079e2d7cc1d12dff038cd3d23905fe9d07d
adrian gherasim <adriangherasim1@gmail.com>
2025-04-29 00:10:25 +0300

    create the infrastructure

--- terraform/environments/azure/dev/variables.tf
+++ terraform/environments/azure/dev/variables.tf
@@ -12,3 +12,18 @@ variable "tags" {
   description = "Common tags."
   type        = map(string)
 }
+
+variable "client_id" {
+  description = "Client id."
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "subscription_id"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "tenant_id"
+  type        = string
+}
