0b5ca5fb6cc5245aa78129c67e3f73019885dd2a
adrian gherasim <adriangherasim1@gmail.com>
2025-04-28 23:56:06 +0300

    create the infrastructure

---
+++ terraform/environments/azure/dev/terraform.tfvars
@@ -0,0 +1,6 @@
+resource_group_name = "dev-rg"
+location            = "westeurope"
+tags = {
+  environment = "dev"
+  owner       = "team"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,4 @@
-variable "aws_region" {}
-variable "gcp_project_id" {}
-variable "gcp_region" {}
-variable "gcp_credentials_file" {}
-variable "azure_location" {}
-variable "azure_resource_group" {}
+variable "resource_group_name" {
+  description = "Azure resource group name."
+  type        = string
+}
