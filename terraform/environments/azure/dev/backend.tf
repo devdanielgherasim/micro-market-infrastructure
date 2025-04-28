19e432ce2939105e17d5bbed7476236f8de5955d
adrian gherasim <adriangherasim1@gmail.com>
2025-04-28 23:59:16 +0300

    create the infrastructure

--- terraform/environments/azure/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-backend-rg"
-    storage_account_name = "terraformstatesa"
-    container_name       = "tfstate"
-    key                  = "azure/dev/terraform.tfstate"
-  }
-}
