ed3803a3d5138338d39f5fd10d333fcd19888683
adrian gherasim <adriangherasim1@gmail.com>
2025-05-04 20:03:20 +0300

    create the infrastructure

--- terraform/environments/azure/dev/variables.tf
+++ terraform/environments/azure/dev/variables.tf
@@ -1,9 +1,19 @@
 variable "client_id" {
   type = string
 }
+
 variable "tenant_id" {
   type = string
 }
+
 variable "subscription_id" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
   type = string
 }
