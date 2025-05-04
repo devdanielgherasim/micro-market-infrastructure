e9d13e4ff042eecb1bf4bbb7c48ee2a3d56ab7c8
adrian gherasim <adriangherasim1@gmail.com>
2025-05-04 20:01:53 +0300

    create the infrastructure

--- terraform/environments/azure/dev/providers.tf
+++ terraform/environments/azure/dev/providers.tf
@@ -1,19 +1,8 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.80.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 
   use_oidc        = true
   client_id       = var.client_id
-  tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
--- terraform/environments/azure/dev/variables.tf
+++ terraform/environments/azure/dev/variables.tf
@@ -7,9 +7,3 @@ variable "tenant_id" {
 variable "subscription_id" {
   type = string
 }
-variable "location" {
-  type = string
-}
-variable "resource_group_name" {
-  type = string
-}
