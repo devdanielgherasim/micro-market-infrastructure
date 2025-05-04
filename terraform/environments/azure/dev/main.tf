fe12e64613845a57f00ddaab42eb763a6143957d
adrian gherasim <adriangherasim1@gmail.com>
2025-05-04 21:48:47 +0300

    create the infrastructure

--- terraform/environments/azure/dev/main.tf
+++ terraform/environments/azure/dev/main.tf
@@ -1,6 +1,6 @@
 module "acr" {
   source              = "../../../modules/azure/container-registry"
-  name                = "${var.resource_group_name}-acr-${var.environment}-${substr(md5(var.client_id), 0, 6)}"
+  name                = "${var.resource_group_name}acr${var.environment}${substr(md5(var.client_id), 0, 6)}"
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = "Standard"
