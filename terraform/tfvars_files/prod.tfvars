09e484d59674339900b5278dfff840f9cb44504d
adrian gherasim <adriangherasim1@gmail.com>
2025-05-19 19:49:31 +0300

    create the infrastructure

--- terraform/tfvars_files/prod.tfvars
+++ terraform/tfvars_files/prod.tfvars
@@ -8,3 +8,23 @@ aks_vm_size  = "Standard_A2_v2"
 nginx_ingress_version       = "4.7.1"
 nginx_ingress_namespace     = "ingress-nginx"
 nginx_ingress_replica_count = 2  # Using 2 replicas for prod environment for high availability
+
+# ArgoCD configuration
+argocd_version       = "5.46.7"
+argocd_namespace     = "argocd"
+argocd_replica_count = 2  # Using 2 replicas for prod environment for high availability
+
+# cert-manager configuration
+cert_manager_version       = "v1.13.2"
+cert_manager_namespace     = "cert-manager"
+cert_manager_replica_count = 2  # Using 2 replicas for prod environment for high availability
+
+# Prometheus configuration
+prometheus_version       = "25.8.0"
+prometheus_namespace     = "monitoring"
+prometheus_replica_count = 2  # Using 2 replicas for prod environment for high availability
+
+# Grafana configuration
+grafana_version       = "7.0.11"
+grafana_namespace     = "monitoring"
+grafana_replica_count = 2  # Using 2 replicas for prod environment for high availability
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -54,3 +54,75 @@ variable "nginx_ingress_replica_count" {
   description = "The number of NGINX Ingress Controller replicas."
   default     = 1
 }
+
+variable "argocd_version" {
+  type        = string
+  description = "The version of the ArgoCD Helm chart."
+  default     = "5.46.7"
+}
+
+variable "argocd_namespace" {
+  type        = string
+  description = "The Kubernetes namespace where ArgoCD will be installed."
+  default     = "argocd"
+}
+
+variable "argocd_replica_count" {
+  type        = number
+  description = "The number of ArgoCD server replicas."
+  default     = 1
+}
+
+variable "cert_manager_version" {
+  type        = string
+  description = "The version of the cert-manager Helm chart."
+  default     = "v1.13.2"
+}
+
+variable "cert_manager_namespace" {
+  type        = string
+  description = "The Kubernetes namespace where cert-manager will be installed."
+  default     = "cert-manager"
+}
+
+variable "cert_manager_replica_count" {
+  type        = number
+  description = "The number of cert-manager controller replicas."
+  default     = 1
+}
+
+variable "prometheus_version" {
+  type        = string
+  description = "The version of the Prometheus Helm chart."
+  default     = "25.8.0"
+}
+
+variable "prometheus_namespace" {
+  type        = string
+  description = "The Kubernetes namespace where Prometheus will be installed."
+  default     = "monitoring"
+}
+
+variable "prometheus_replica_count" {
+  type        = number
+  description = "The number of Prometheus server replicas."
+  default     = 1
+}
+
+variable "grafana_version" {
+  type        = string
+  description = "The version of the Grafana Helm chart."
+  default     = "7.0.11"
+}
+
+variable "grafana_namespace" {
+  type        = string
+  description = "The Kubernetes namespace where Grafana will be installed."
+  default     = "monitoring"
+}
+
+variable "grafana_replica_count" {
+  type        = number
+  description = "The number of Grafana server replicas."
+  default     = 1
+}
