75c7e3bc763ed9dde99ade68a9cd34a857710410
adrian gherasim <adriangherasim1@gmail.com>
2025-06-16 12:57:59 +0300

    update infra

--- terraform/kubernetes/update_azure_network.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f ./configs/production_issuer.yaml
-
-CI_PROJECT_NAMESPACE="microservices1691717"
-PUBLIC_IP_NAME=$(az network public-ip list --resource-group rg-$CI_PROJECT_NAMESPACE-dev-aks --query "[?contains(name, 'kubernetes-')].name" -o tsv)
-az network public-ip update --resource-group rg-$CI_PROJECT_NAMESPACE-dev-aks --name $PUBLIC_IP_NAME --dns-name $CI_PROJECT_NAMESPACE
