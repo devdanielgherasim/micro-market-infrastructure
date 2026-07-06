b3e3119bcaf4a53f8e3b36576c07ea835da96ba9
danieladriangherasim <adriangherasim1@gmail.com>
2026-07-06 17:54:00 +0300

    updates

--- terraform/aws/roles.sh
+++ terraform/aws/roles.sh
@@ -4,12 +4,15 @@ set -euo pipefail
 
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-aws eks create-access-entry --cluster-name eks-danielgherasim-microservices-dev --principal-arn arn:aws:iam::562346647958:user/daniel-admin --region eu-central-1
+aws eks create-access-entry \
+  --cluster-name eks-danielgherasim-microservices-dev \
+  --principal-arn arn:aws:iam::${AWS_ACCOUNT_ID}:user/daniel-admin \
+  --region eu-central-1
 
   # Grant cluster-admin level access
-  aws eks associate-access-policy \
-    --cluster-name eks-danielgherasim-microservices-dev \
-    --principal-arn arn:aws:iam::${AWS_ACCOUNT_ID}:user/daniel-admin \
-    --policy-arn arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy \
-    --access-scope type=cluster \
-    --region eu-central-1
+aws eks associate-access-policy \
+  --cluster-name eks-danielgherasim-microservices-dev \
+  --principal-arn arn:aws:iam::${AWS_ACCOUNT_ID}:user/daniel-admin \
+  --policy-arn arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy \
+  --access-scope type=cluster \
+  --region eu-central-1
